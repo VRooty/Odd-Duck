d56ddba7f538d80f63f9e072ab399e267bbf1909
Vrooty <victoriadohertybone@live.co.uk>
2023-10-04 16:03:11 +0100

    add new stuff

--- app.js
+++ app.js
@@ -32,51 +32,49 @@ const products = [
     return Math.floor(Math.random() * products.length);
   }
 
-  function renderProducts() {
+function renderProducts() {
     
-    let prod1 = randomProdIdx();
-    let prod2 = randomProdIdx();
-    let prod3 = randomProdIdx();
+  let prod1 = randomProdIdx();
+  let prod2 = randomProdIdx();
+  let prod3 = randomProdIdx();
 
-    while (prod1 === prod2 || prod1 === prod3 || prod2 === prod3) {
-        prod2 = randomProdIdx();
-        prod3 = randomProdIdx();
-      }
-
-
+  while (prod1 === prod2 || prod1 === prod3 || prod2 === prod3) {
+      prod2 = randomProdIdx();
+      prod3 = randomProdIdx();
     }
-
-img1.src = products[prod1].src;
-img2.src = products[prod2].src;
-img3.src = products[prod3].src;
-img1.alt = products[prod1].name;
-img2.alt = products[prod2].name;
-img3.alt = products[prod3].name;
-
+  img1.src = products[prod1].src;
+  img2.src = products[prod2].src;
+  img3.src = products[prod3].src;
+  img1.alt = products[prod1].name;
+  img2.alt = products[prod2].name;
+  img3.alt = products[prod3].name;
 
 
-products[prod1].views++;
-products[prod2].views++;
-products[prod3].views++;
+
+  products[prod1].views++;
+  products[prod2].views++;
+  products[prod3].views++;
+
+}
+
+
 
 function handleImgClick(event) {
     
     if (userClicks === maxClicks) {
       alert("You have run out of votes");
+      showResults()
       return;
-
-user++;
-
     }
+    let clickedProduct = event.target.alt;
 
+    for (let i = 0; i < products.length; i++) {
+      if (clickedProduct === products[i].name) {
+        userClicks++
+        products[i].clicks++;
+      }
 }
 
-let ClickedProduct = event.target.alt;
-
-for (let i = 0; i < products.length; i++) {
-    if (clickedProduct === products[i].name) {
-      products[i].clicks++;
-    }
 
 
 renderProducts();
@@ -91,19 +89,19 @@ function showResults() {
 
     const results = document.getElementById("results");
 
+    for (let i = 0; i < products.length; i++) {
+      const li = document.createElement("li");
+      const product = products[i];
+      li.textContent = `${product.name} was viewed ${product.views} times, and clicked ${product.clicks} times`;
+      results.appendChild(li);
+  
+  }
+  
+  const viewResults = document.getElementById("view-results");
+  viewResults.addEventListener("click", showResults);
 
 }
 
-for (let i = 0; i < products.length; i++) {
-    const li = document.createElement("li");
-    const product = products[i];
-    li.textContent = `${product.name} was viewed ${product.views} times, and clicked ${product.clicks} times`;
-    results.appendChild(li);
-
-}
-
-const viewResults = document.getElementById("view-results");
-viewResults.addEventListener("click", showResults);
 
 renderProducts();
 
@@ -123,8 +121,6 @@ function showResults() {
   }
 }
 
-const viewResults = document.getElementById("view-results");
-viewResults.addEventListener("click", showResults);
 
 renderProducts();
 
